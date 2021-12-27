a99a7044c03f6af2ae8fa33623f700a75509f552
lschulzes <lucassilvaiee@gmail.com>
2021-12-27 16:25:21 -0300

    Complie contract through node

--- complie.ts
+++
@@ -1,7 +0,0 @@
-import * as path from "path";
-import * as fs from "fs";
-import { compileSol } from "solc-typed-ast";
-
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf-8");
-console.log(compileSol(source, "auto", []));
