d1e9348aa5774b0ae73cf27427ea4e3cb0e2fb5b
lschulzes <lucassilvaiee@gmail.com>
2021-12-27 18:11:17 -0300

    Add simple tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,19 +5,33 @@ const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
-class Car {
-  park() {
-    return "stopped";
-  }
+const { abi, evm } = require("../compile");
 
-  drive() {
-    return "vroom";
-  }
-}
+const message = "Hi there!";
+let accounts;
+let inbox;
 
-describe("Car", () => {
-  it("can park", () => {
-    const car = new Car();
-    assert.equal(car.park(), "stopped");
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({ data: "0x" + evm.bytecode.object, arguments: [message] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("General test", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const messageResponse = await inbox.methods.message().call();
+    assert.equal(message, messageResponse);
+  });
+
+  it("Can change the message", async () => {
+    await inbox.methods.setMessage("Bye There!").send({ from: accounts[0] });
+    const messageChange = await inbox.methods.message().call();
+    assert.notEqual(message, messageChange);
+    assert.equal("Bye There!", messageChange);
   });
 });
